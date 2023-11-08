e5233c2168d3df1e8b0b8d17e7c7614fc0ea65af
Seonyeong12 <125814031+Seonyeong12@users.noreply.github.com>
2023-11-08 16:24:38 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-
+document.getElementById("convertButton").addEventListener("click", function () {
+    chrome.scripting.executeScript({
+      target: { tabId: 1 },
+      function: convertToEmoticon
+    });
+  });
+  #hello를 이모티콘으로 변환하는 코드
+  function convertToEmoticon() {
+    let text = document.body.textContent;
+    text = text.replace(/Hello/g, "😀");
+    document.body.textContent = text;
+  }
+  
