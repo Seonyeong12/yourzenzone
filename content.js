a9b5feb32c49cbeb9118737698fc4f92e3f2c48c
Seonyeong12 <125814031+Seonyeong12@users.noreply.github.com>
2023-11-08 16:25:06 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,20 @@
+// 예를 들어, "hate speech"와 "offensive term"을 감지하는 코드
+const offensiveWords = ["hate speech", "offensive term"];
+const regex = new RegExp(offensiveWords.join("|"), "gi"); // 대소문자 무시, 전체 매치
 
+const text = document.body.textContent;
+if (regex.test(text)) {
+  // 혐오 표현을 발견하면 대체 작업을 수행
+}
+
+const emoticons = ["😃", "😊", "😄", "😁", "😆", "😅"];
+function getRandomEmoticon() {
+  const randomIndex = Math.floor(Math.random() * emoticons.length);
+  return emoticons[randomIndex];
+}
+
+if (regex.test(text)) {
+    const replacedText = text.replace(regex, getRandomEmoticon());
+    document.body.textContent = replacedText;
+  }
+  
