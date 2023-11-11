066dada36fb588b548a48825938998f8f52bc195
Seonyeong12 <125814031+Seonyeong12@users.noreply.github.com>
2023-11-11 23:25:43 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,6 +4,15 @@ document.getElementById("convertButton").addEventListener("click", function () {
       function: convertToEmoticon
     });
   });
+
+//content.js에서 문장 단위로 parse하기 위한 버튼
+//후에 버튼 이름을 수정하거나, 필터링 버튼을 눌렀을 때 자동으로 같이 진행되도록 변경 필요해보임.
+document.getElementById('parseButton').addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'parseHTML' });
+  });
+});
+
   //hello를 이모티콘으로 변환하는 코드
   function convertToEmoticon() {
     let text = document.body.textContent;
