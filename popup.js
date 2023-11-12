9b5a392f84109a4ac4b9a37d22fbac99e2fd9618
Jeonghyun66 <125812355+Jeonghyun66@users.noreply.github.com>
2023-11-12 20:36:32 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -18,6 +18,32 @@ function updateHtmlOnPage(htmlInfo) {
   document.body.innerHTML = htmlInfo;
     //이는 웹 페이지를 간단히 업데이트하는 방법 중 하나이지만, 모든 시나리오에 적합하지 않을 수 있습니다.
 }
+// 수정된 부분: content.js에서 최종 결과를 받아와 혐오 표현을 랜덤 이모티콘으로 대체하고 웹 페이지를 업데이트하는 함수
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'displaySentences') {
+    const labeledSentences = request.sentences;
+    replaceHateSentencesWithEmoticons(labeledSentences);
+    sendUpdatedHtmlToWebPage();
+  }
+});
+
+// 수정된 부분: 최종 결과에서 혐오 표현을 랜덤 이모티콘으로 대체하는 함수
+function replaceHateSentencesWithEmoticons(labeledSentences) {
+  for (const labeledSentence of labeledSentences) {
+    const { sentence, label } = labeledSentence;
+    if (label === 'hate speech' || label === 'offensive term') {
+      const replacedSentence = sentence.replace(regex, getRandomEmoticon());
+      document.body.innerHTML = document.body.innerHTML.replace(sentence, replacedSentence);
+    }
+  }
+}
+
+// 수정된 부분: 최종 결과에서 변경된 HTML 정보를 다시 웹 페이지로 전송하는 함수
+function sendUpdatedHtmlToWebPage() {
+  const updatedHtml = document.documentElement.outerHTML;
+  chrome.runtime.sendMessage({ action: "updateHtml", htmlInfo: updatedHtml });
+}
+
 
 // 필요한 경우 추가적인 함수 또는 로직을 추가하세요
 //content.js에서 문장 단위로 parse하기 위한 버튼
