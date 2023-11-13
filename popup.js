9fedd5b0b54f4fae6892159c89ce070e7b8a6ca5
ella0106 <ella0106@naver.com>
2023-11-13 13:46:06 +0900

    add checkbox

--- popup.js
+++ popup.js
@@ -75,3 +75,29 @@ document.getElementById('parseButton').addEventListener('click', function () {
     document.body.textContent = text;
   }
   
+
+
+  const checkboxes = document.querySelectorAll('.category_check');
+
+function getCheckedOptions() {
+  // 체크된 체크박스 요소들을 선택
+  const checkboxStatus = [];
+
+  // 각 체크박스의 값을 배열에 추가
+  checkboxes.forEach(checkbox => {
+    checkboxStatus.push(checkbox.checked);
+  });
+
+  // 배열 출력
+  console.log('Checkbox Status:', checkboxStatus);
+
+  return checkboxStatus
+}
+
+getCheckedOptions()
+  
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+    getCheckedOptions()
+  });
+});
