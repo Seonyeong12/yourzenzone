245b58da38c08f33fc473aaf92f56b63f842eb2a
ella0106 <ella0106@naver.com>
2023-11-14 14:22:02 +0900

    update

--- popup.js
+++ popup.js
@@ -58,16 +58,6 @@ function sendUpdatedHtmlToWebPage() {
 }
 
 
-// 필요한 경우 추가적인 함수 또는 로직을 추가하세요
-//content.js에서 문장 단위로 parse하기 위한 버튼
-//후에 버튼 이름을 수정하거나, 필터링 버튼을 눌렀을 때 자동으로 같이 진행되도록 변경 필요해보임.
-// content.js에서의 HTML 파싱 기능을 트리거합니다
-document.getElementById('parseButton').addEventListener('click', function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'parseHTML' });
-  });
-});
-
   //hello를 이모티콘으로 변환하는 코드
   function convertToEmoticon() {
     let text = document.body.textContent;
