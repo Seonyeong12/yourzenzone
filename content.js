1b2e25384e24576eab3d4177a08ffcad99e47a2c
ella0106 <ella0106@naver.com>
2023-11-20 16:04:49 +0900

    update css

--- content.js
+++ content.js
@@ -1,17 +1,4 @@
 const body = document.querySelector('body');
-// const CheckboxStatus = chrome.storage.sync.get("checkboxStatus");
-// const keywords = chrome.storage.sync.get("keywords");
-// console.log(CheckboxStatus, keywords.keywords);
-
-chrome.storage.onChanged.addListener((changes, namespace) => {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
-    );
-  }
-});
-
 
 const emoticons = ["😃", "😊", "😄", "😁", "😆", "😅"];
 function getRandomEmoticon() {
@@ -19,57 +6,66 @@ function getRandomEmoticon() {
   return emoticons[randomIndex];
 }
 
-function preprocess(txt){
+function preprocess(txt) {
   var t = txt.replaceAll(/\s{2,}/g, ' ');
-  var t = t.replaceAll('\n', '');
-  var t = t.replaceAll('\t', '');
+  t = t.replaceAll('\n', '');
+  t = t.replaceAll('\t', '');
   return t;
 }
 
-const TagList = ['SCRIPT', 'STYLE', 'NOSCRIPT', 'BODY','FORM'];
+const TagList = ['SCRIPT', 'STYLE', 'NOSCRIPT', 'BODY', 'FORM'];
 const pattern = /^[^ㄱ-ㅎ가-힣a-zA-Z]*$/;
 let nodeList = [];
 let contentList = [];
+
 function getNodeList(element) {
   if (element.nodeType === Node.TEXT_NODE) {
-    if (TagList.includes(element.parentElement.tagName)) {
-      ;
-    } else {
+    if (!TagList.includes(element.parentElement.tagName)) {
       var t = preprocess(element.textContent);
-      if (t.match(pattern)){
-        ;
-      }
-      else{
-        nodeList.push(element.parentElement);
-        contentList.push([t])
+      if (!t.match(pattern)) {
+        nodeList.push(element); // element를 직접 저장
+        contentList.push(t);
       }
     }
-    // If the current node is a text node, replace text content
   } else if (element.nodeType === Node.ELEMENT_NODE) {
-    // If the current node is an element, recursively process its children
     for (const child of element.childNodes) {
       getNodeList(child);
     }
   }
-  return nodeList, contentList;
 }
 
-function ChangeTo(keyword){
-nodeList.forEach(element => {
-  const originalText = element.textContent;
-  const newText = originalText.replace(keyword, getRandomEmoticon());
-  element.textContent = keyword;
+function ChangeTo(keyword) {
+  nodeList.forEach(element => {
+    const originalText = element.textContent;
+    const regex = new RegExp(keyword);
+    const newText = originalText.replace(regex, getRandomEmoticon());
+    element.textContent = newText;
+    //element.textContent = getRandomEmoticon();
+  });
+}
+
+function processPage() {
+  chrome.storage.sync.get(["keywords"], function(result) {
+    if (result.keywords) {
+      getNodeList(body);
+      //console.log(nodeList);
+      result.keywords.forEach(keyword => {
+        ChangeTo(keyword);
+      });
+    }
+  });
+}
+
+processPage();
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    if (key === 'keywords') {
+      processPage(); // 키워드가 변경되면 페이지를 다시 처리합니다.
+    }
   }
-)};
+});
 
-
-chrome.storage.sync.get(["keywords"], function(result){
-  getNodeList(body);
-  console.log(nodeList);
-  console.log(result.keywords);
-  //chrome.storage.local.set({content:contentList})
-  result.keywords.forEach(keyword => {
-    console.log(keyword);
-    ChangeTo(keyword);
-  })
-})
+window.addEventListener('scroll', () => {
+  processPage();
+});
